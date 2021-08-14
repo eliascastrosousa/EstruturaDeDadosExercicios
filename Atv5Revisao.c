a8075f0be1ae3840d9c101ae62d5ce34a1a06c7a
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-14 20:41:57 -0300

    commit atv4 em andamento

--- Atv5Revisao.c
+++ Atv5Revisao.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+#define TAM 5
 
+main(){
+setlocale(LC_ALL,"portuguese");
+char v[TAM];
+
+printf("Digite seu nome: ");
+scanf("%s", &v[TAM]);
+
+printf("\nSeu nome é %s", v[TAM]);
+
+
+
+}
