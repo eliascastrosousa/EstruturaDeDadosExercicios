2f7b8081034bc59893905e94ecfb6fb409623f16
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-19 21:11:08 -0300

    commit

--- Atv6Revisao.c
+++ Atv6Revisao.c
@@ -24,10 +24,11 @@ printf("A Frase tem %d Caracteres e %d espaço(s) em branco. ", strlen(frase) ,es
 for(c=0; c<strlen(frase);c++){
     if(frase[c] == ' '){
             frase[c]=frase[c+1];
+            //frase[c]=frase[c];
     }
 }
 
-printf("\nO texto sem espaços em branco fica assim: %s",frase);
+printf("\nO texto sem espaços em branco fica: %s",frase);
 
 //for(c=0; c<strlen(frase);c++){ printf("%c", frase[c]);}
 
