6a89dc7d0e8cdd090c7bbf7918db3b3ceda7b64e
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-23 20:55:19 -0300

    commit

--- Lista1ex5.c
+++ Lista1ex5.c
@@ -1,1 +1,17 @@
+/* 5. Efetue a leitura de três valores e apresente como resultado a soma dos quadrados dos três valores lidos */
 
+#include <stdio.h>
+#include <locale.h>
+
+main(){
+float n1,quad;
+
+setlocale(LC_ALL, "portuguese");
+
+printf("Digite o numero real: ");
+scanf("%f", &n1);
+
+quad = n1*n1;
+
+printf("\nO numero %0.2f ao quadrado é: %0.2f",n1,quad);
+}
