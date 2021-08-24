a0f37b5bfc12956881c7cc1cb8f6f50c175bad46
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-24 17:45:03 -0300

    commit ex 12 e 14

--- Lista1ex12.c
+++ Lista1ex12.c
@@ -1,1 +1,28 @@
 
+/* . Sejam 𝑎 e 𝑏 os catetos de um triângulo, onde a hipotenusa é obtida pela equação:
+ℎ𝑖𝑝𝑜𝑡𝑒𝑛𝑢𝑠𝑎 = √𝑎
+2 + 𝑏
+2. Faça um programa que receba os valores de 𝑎 e 𝑏 e encontre o
+valor da hipotenusa através da equação. Imprima no final o resultado dessa operação. */
+
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+float cata,catb,hip;
+
+main(){
+
+setlocale(LC_ALL,"portuguese");
+
+printf("Digite o valor do cateto A: ");
+scanf("%f",&cata);
+printf("Digite o valor do cateto B: ");
+scanf("%f",&catb);
+
+hip = (cata*cata)+(catb*catb);
+hip = sqrt(hip);
+
+printf("o valor da hipotenusa é igual a: %0.2f",hip);
+
+}
--- Lista1ex14.c
+++ Lista1ex14.c
@@ -1,1 +1,20 @@
+/* 14. Faça um programa que possa entrar com o valor de um produto e imprima o valor tendo
+em vista que o desconto foi de 12% */
 
+#include <stdio.h>
+#include <locale.h>
+
+float val,desc;
+
+main(){
+setlocale(LC_ALL,"portuguese");
+
+printf("Digite o valor $$ do produto: ");
+scanf("%f",&val);
+
+desc = val*12/100;
+desc = val-desc;
+
+printf("o valor R$%0.2f com desconto de 12%% é igual a: R$%0.2f",val,desc);
+
+}
