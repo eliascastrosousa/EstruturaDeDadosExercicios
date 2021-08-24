b9dbc390a45b5f66f6eaf24e73a17be604b78d74
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-24 17:32:59 -0300

    commit ex 10,11

--- Lista1ex10.c
+++ Lista1ex10.c
@@ -1,1 +1,19 @@
 
+/* 10. Leia o tamanho do lado de um quadrado e imprima como resultado a sua área. */
+
+#include <stdio.h>
+#include <locale.h>
+
+float lado, area;
+
+main(){
+setlocale(LC_ALL,"portuguese");
+
+printf("Digite o lado do quadrado: ");
+scanf("%f",&lado);
+
+area = lado*lado;
+
+printf("A área do quadrado é igual a: %0.2fm²",area);
+
+}
--- Lista1ex11.c
+++ Lista1ex11.c
@@ -1,1 +1,21 @@
 
+/* . Leia o valor do raio de um círculo e calcule a área do círculo correspondente. Imprima o
+resultado dessa operação. A área do círculo é 𝜋 ∗ 𝑟𝑎𝑖𝑜², considere 𝜋 = 3,141592 . */
+
+#include <stdio.h>
+#include <locale.h>
+
+float raio, area;
+
+main(){
+
+setlocale(LC_ALL, "portuguese");
+
+printf("Digite o valor do raio de um circulo: ");
+scanf("%f",&raio);
+
+area = (raio*raio)*3,14;
+
+printf("A área do circulo é igual a: %0.2f",area);
+
+}
