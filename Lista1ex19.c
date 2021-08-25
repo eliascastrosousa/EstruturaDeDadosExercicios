804d80d71e26da81b5eee1d68a536382939526fe
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-25 16:42:01 -0300

    commit novos exercicios

--- Lista1ex19.c
+++ Lista1ex19.c
@@ -1,1 +1,23 @@
+/*19. Receba o salário-base de um funcionário, calcule e imprima o salário a receber, sabendo-se
+que esse funcionário tem uma gratificação de 5% sobre o salário-base. Além disso, paga 7% de
+imposto sobre o salário-base.
+*/
 
+#include <stdio.h>
+#include <locale.h>
+
+main(){
+setlocale(LC_ALL,"portuguese");
+int horas;
+float salario,desconto,sal_total;
+
+printf("Digite o Salario Base: ");
+scanf("%f",&salario);
+
+sal_total = salario*1.05;
+desconto = sal_total*7/100;
+
+printf("\nSalario Base: R$%0.2f \nSalario com Gratificação: R$%0.2f \nTotal a Pagar (Com dedução de imposto): R$%0.2f",salario,sal_total,sal_total-desconto);
+
+
+}
