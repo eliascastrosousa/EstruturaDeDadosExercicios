628e48425faa5e3758f4079038a57a23aae3f988
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-24 22:56:07 -0300

    commit 15 e 16

--- Lista1ex15.c
+++ Lista1ex15.c
@@ -1,1 +1,21 @@
+/*15. Receba o salário de um funcionário, calcule e imprima o valor do novo salário, sabendo que
+ele recebeu um aumento de 25%. */
+
+#include <stdio.h>
+#include <locale.h>
+
+float val,aum;
+
+main(){
+setlocale(LC_ALL,"portuguese");
+
+printf("Digite o valor $$ do produto: ");
+scanf("%f",&val);
+
+aum = val*1.25;
+//aum = ;
+
+printf("o valor R$%0.2f com aumento de 25%% é igual a: R$%0.2f",val,aum);
+
+}
 
--- Lista1ex16.c
+++ Lista1ex16.c
@@ -1,1 +1,19 @@
+/*16. A importância de R$ 780.000,00 será dividida entre três ganhadores de um concurso.
+Sendo que da quantia total:
+ O primeiro ganhador receberá 46%;
+ O segundo receberá 32%;
+ O terceiro receberá o restante;
+Calcule e imprima a quantia ganha por cada um dos ganhadores. */
 
+#include <locale.h>
+#include <stdio.h>
+
+float premio=780;
+
+main(){
+
+setlocale(LC_ALL,"portuguese");
+
+printf("O primeiro ganhador receberá 46%% equivalente a: %0.2f \n\nO segundo receberá 32%% equivalente a: %0.2f \n\nO terceiro receberá o restante equivalente a: %0.2f\n\n",(premio*1.46)-premio,(premio*1.32)-premio,(premio*1.22)-premio);
+
+}
