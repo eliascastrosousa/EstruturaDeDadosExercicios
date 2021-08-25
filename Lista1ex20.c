98371d7c1e9bbd1103d87d8440d55ca48c731ef1
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-25 17:32:17 -0300

    commit 20,21

--- Lista1ex20.c
+++ Lista1ex20.c
@@ -1,1 +1,24 @@
+/*20. Escreva um algoritmo para criar um programa de ajuda para vendedores. A partir de um
+valor total lido, mostre:
+ O total a pagar com desconto de 10%;
+ O valor de cada parcela, no parcelamento de 3 x sem juros;
+ A comissão do vendedor, no caso de a venda ser a vista (5% sobre o valor com desconto)
+ A comissão do vendedor, no caso de a venda ser parcelada (5% sobre o valor total)
+*/
+#include <stdio.h>
+#include <locale.h>
 
+main(){
+setlocale(LC_ALL,"portuguese");
+float valor,desconto,comissao;
+
+printf("Digite o valor: ");
+scanf("%f",&valor);
+desconto = valor*10/100;
+
+printf("\nO total a pagar com desconto de 10%: R$%0.2f",valor-desconto);
+printf("\nO valor de cada parcela, no parcelamento de 3 x sem juros: R$%0.2f",valor/3);
+printf("\nA comissão do vendedor caso de a venda seja a vista: R$%0.2f",(valor-desconto)*0.05);
+printf("\nA comissão do vendedor, no caso de a venda ser parcelada: R$%0.2f\n\n",valor*0.05);
+
+}
--- Lista1ex21.c
+++ Lista1ex21.c
@@ -1,1 +1,22 @@
+/* 21. Receba a altura do degrau de uma escada e a altura que o usuário deseja alcançar subindo
+a escada. Calcule e mostre quantos degraus o usuário deverá subir para atingir seu objetivo,
+sem se preocupar com a altura do usuário. */
 
+#include <stdio.h>
+#include <locale.h>
+
+main(){
+setlocale(LC_ALL,"portuguese");
+float altura,degrau,total;
+
+printf("Digite a altura do degrau da escada: ");
+scanf("%f",&degrau);
+
+printf("Qual a altura que o usuário deseja alcançar subindo: ");
+scanf("%f",&altura);
+
+total = altura/degrau;
+
+printf("\nDevera ser construido %0.0f degrau(s) para alcançar a altura desejada.\n",total);
+
+}
