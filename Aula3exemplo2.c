66eb1f7a58de870d87119ee6eebe2867e818f94a
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-28 17:40:35 -0300

    commit

---
+++ Aula3exemplo2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct pessoa{ //instrução
+    char nome;
+    int idade;
+};
+int main(){
+struct pessoa p;
+
+strcpy(p.nome, "José da Silva");
+p.idade = 15;
+
+
+
+}
+
+
