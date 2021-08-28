a2be4933bee79f416408f77c2734a2e0103918da
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-28 16:00:13 -0300

    commit

--- Aula3ex1.c
+++ Aula3ex1.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define tam 2
+#define tam 3
 
 struct funcionario{
     int id;
@@ -14,20 +14,25 @@ void main(){
 struct funcionario func[tam]; //func1, func2, func3, func4, func5;
 
 for(c=0;c<tam;c++){
-    printf("\nDigite o nome do funcionario [%d]: ",c+1);
-    fgets(func[c].nome,30,stdin);
 
     printf("Digite o ID do funcionario [%d]: ",c+1);
     scanf("%d",&func[c].id);
 
+    getchar();
+    printf("Digite o nome do funcionario [%d]: ",c+1);
+    fgets(func[c].nome,30,stdin);
+
     printf("Digite a idade do funcionario [%d]: ",c+1);
     scanf("%d",&func[c].idade);
+    printf("\n\n");
 }
 
 for(c=0;c<tam;c++){
-    printf("\n\nNome do funcionario [%d]: %s",c+1,func[c].nome);
+    printf("\n----------------------------------");
     printf("\nID do funcionario [%d]: %d",c+1,func[c].id);
-    printf("\nIdade do funcionario [%d]: %d",c+1,func[c].idade);
+    printf("\nNome do funcionario [%d]: %s",c+1,func[c].nome);
+    printf("Idade do funcionario [%d]: %d",c+1,func[c].idade);
+    printf("\n----------------------------------\n\n");
 }
-printf("\n\n");
+printf("\n\n\n");
 }
