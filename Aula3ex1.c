8b257f08b6f320a8a3f8092437d05c856867cf13
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-28 12:58:33 -0300

    commt

--- Aula3ex1.c
+++ Aula3ex1.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define tam 3
+#define tam 2
 
 struct funcionario{
     int id;
     int idade;
     char nome[30];
 };
+int c=0;
 void main(){
 struct funcionario func[tam]; //func1, func2, func3, func4, func5;
 
-for(int c=0;c<tam;c++){
+for(c=0;c<tam;c++){
     printf("\nDigite o nome do funcionario [%d]: ",c+1);
     fgets(func[c].nome,30,stdin);
 
@@ -25,7 +26,7 @@ for(int c=0;c<tam;c++){
 system("pause");
 
 
-for(int c=0;c<tam;c++){
+for(c=0;c<tam;c++){
     printf("\n\nNome do funcionario [%d]: %s",c+1,func[c].nome);
     printf("\nID do funcionario [%d]: %d",c+1,func[c].id);
     printf("\nIdade do funcionario [%d]: %d",c+1,func[c].idade);
