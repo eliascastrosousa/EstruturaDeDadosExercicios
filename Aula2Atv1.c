4085b1e028a1f7b7ab035a6540ac052b0cf01efb
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-29 17:12:20 -0300

    commit exemplo 5

--- Aula2Atv1.c
+++ Aula2Atv1.c
@@ -11,7 +11,7 @@ float CalculoDeducoes(float salario_h,int numero_f);
 float sal_hora, sal_filho, resultado1,resultado2;
 int num_horas, num_filhos;
 
-void main(){
+int main(){
 
 printf("Digite o Valor da Hora/Salario: ");
 scanf("%f",&sal_hora);
@@ -27,15 +27,18 @@ scanf("%f",&sal_filho);
 
 resultado1 = CalculoVantagens(sal_hora, num_horas, num_filhos, sal_filho);
 resultado2 = CalculoDeducoes(sal_hora,num_horas);
+
+return 0;
 }
 
-//chamada do modulo
+//chamada do modulo vantagens
 float CalculoVantagens(float salario_h,int numero_h ,int numero_f ,float salario_f ){
     float salario_b = numero_h*salario_h, salario_fam = (numero_f*salario_f), vantagens = salario_b+salario_fam;
     printf("\n\n------------------------------------");
     printf("\nO valor do Salario e: R$R$%0.2f \nO valor do Salario Familia e de: R$%0.2f \nTotal de Vantagens: R$%0.2f\n\n",salario_b, salario_fam, vantagens);
 }
 
+//chamada do modulo descontos
 float CalculoDeducoes(float salario_h,int numero_f){
     float salario_b = salario_h*numero_f, INSS = salario_b*0.08, IRPF = salario_b*0.12, deducoes = INSS+IRPF;
 
@@ -43,8 +46,6 @@ float CalculoDeducoes(float salario_h,int numero_f){
     printf("\nValor do INSS a ser pago: R$%0.2f \nValor do IRPR a ser pago: R$%0.2f \nTotal de deducoes e de: R$%0.2f",INSS,IRPF,deducoes);
     printf("",IRPF);
     printf("\n------------------------------------\n\n");
-
-
 }
 
 
---
+++ Aula2Exemplo5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void troca(int *x, int *y); //criação do metodo
+
+
+int main(){
+
+    int a, b;
+    scanf("%d %d", &a,  &b);
+
+    troca(&a, &b); //chamada do metodo dando valor aos procedimentos
+
+    printf("a: %d, b: %d\n", a,b);
+
+//return 0;
+}
+
+//escopo do metodo troca
+void troca(int *x, int *y){
+    int temp; // <- variavel local
+
+    temp = *x ; // <- recebe o valor de x
+    *x = *y ; // <- x recebe o valor de y
+    *y = temp ; // <- y recebe o valor de temp
+}
