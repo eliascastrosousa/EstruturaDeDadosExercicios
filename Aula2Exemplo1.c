36af2dbe61a11535e1f5bf87ff3a21cf95c1e484
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-08-28 21:03:17 -0300

    cm

---
+++ Aula2Exemplo1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+int calculatotal(int num1 , int num2); //DEFINO O MODULO E SUAS VARIAVEIS
+
+int main(){
+setlocale(LC_ALL,"portuguese");
+
+int x=5, y=6, resultado=0;
+resultado = calculatotal(x,y); // AQUI ATRIBUO AO MODULO UM VALOR AS SUAS VARIAVEIS
+
+printf("Resultado é %d!!\n",resultado);
+
+}
+
+int calculatotal(int num1, int num2){ // O PROGRAMA PULA PRA CÁ E FAZ O CALCULO DO MODULO
+        return (num1+num2); // RETORNA A SOMA COMO RESULT PARA A VARIAVEL RESULTADO.
+}
+
---
+++ Aula2Exemplo2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+char calculatotal(char nome , char sobrenome);
+
+int main(){
+setlocale(LC_ALL,"portuguese");
+
+char nome_aluno[10], sobrenome_aluno[10], sobrenome_completo[20];
+
+printf("Digite um nome: ");
+fgets(nome_aluno,10,stdin);
+printf("Digite um sobrenome: ");
+fgets(sobrenome_aluno,10,stdin);
+sobrenome_completo[20] = calculatotal(nome_aluno,sobrenome_aluno);
+printf("Nome completo: ",sobrenome_completo);
+}
+
+ char calculatotal(char nome , char sobrenome){
+    return strcpy(nome + sobrenome);
+}
+
+
