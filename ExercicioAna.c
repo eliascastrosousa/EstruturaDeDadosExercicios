7f79e1b8bbd85e6614b737c9a9601b9317306de0
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-09-03 15:17:48 -0300

    commit

--- ExercicioAna.c
+++ ExercicioAna.c
@@ -11,34 +11,35 @@ struct funcionario{
 
 void main()
 {
+
+setlocale(LC_ALL,"Portuguese");
+
     int i;
     struct funcionario colaboradores[5];
     setlocale(LC_ALL, "Portuguese");
 
     //armazena dados do funcionário
-    for(i = 0; i < 5; i++){
+    for(i = 0; i < 2; i++){
 
-        printf("\n   Digite o ID do funcionário %d: ", i+1);
-        scanf("%d",colaboradores[i].id);
-        fflush(stdin);
-        printf("\n   Digite o nome do funcionário %d: ", i+1);
+        printf("\n   Digite o ID do funcioario %d: ", i+1);
+        scanf("%d",&colaboradores[i].id);
+        getchar();
+        printf("\n   Digite o nome do funcioario %d: ", i+1);
         fgets(colaboradores[i].nome, 29, stdin);
-        printf("\n   Digite o salário do funcionário %d: ", i+1);
+        printf("\n   Digite o salario do funcioario %d: ", i+1);
         scanf("%f",&colaboradores[i].salario);
-        printf("\n   Digite a idade do funcionário %d: ", i+1);
-        scanf("%d",colaboradores[i].idade);
-
-
+        printf("\n   Digite a idade do funcioario %d: ", i+1);
+        scanf("%d",&colaboradores[i].idade);
     }
     // imprime dados do funcionário
-   for(i = 0; i < 5; i++){
+   for(i = 0; i < 2; i++){
         printf("\n   =======================================");
-        printf("\n         FUNCIONÁRIO %d ", i);
+        printf("\n         FUNCIONARIO %d ", i+1);
         printf("\n   =======================================");
-        printf("\n   O ID do funcionário %d é: %d", i, colaboradores[i].id);
-        printf("\n   O nome do funcionário %d é: %s", i,colaboradores[i].nome);
-        printf("\n   A idade do funcionário %d é de: %d anos", i,colaboradores[i].idade);
-        printf("\n   O salário do funcionário %d é: %f", i,colaboradores[i].salario);
+        printf("\n   O ID do funcioario %d e: %d", i+1, colaboradores[i].id);
+        printf("\n   O nome do funcioario %d e: %s", i+1,colaboradores[i].nome);
+        printf("\n   A idade do funcioario %d e de: %d anos", i+1,colaboradores[i].idade);
+        printf("\n   O salario do funcioario %i e: %0.2f", i+1,colaboradores[i].salario);
      }
 
 }
