4953d2667ffebbe6dafb5491df40500e32fa9e4e
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-09-12 15:26:31 -0300

    commit

--- Lista1ex35.c
+++ Lista1ex35.c
@@ -1,1 +1,21 @@
+/*36. Ler uma velocidade em km/h (quilômetros por hora) e apresentá-la convertida em 𝑚/𝑠
+(metros por segundo). A fórmula de conversão é: 𝑀 = 𝑘/3,6, sendo 𝐾 a velocidade em 𝑘𝑚/ℎ
+e 𝑀 em m/s.
+*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(int argc, char const *argv[]){
+	float km,ms;
+
+	printf("Digite a velocidade em km/h (quilometros por hora): ");
+	scanf("%f",&km);
+
+	ms = km/3.6;
+
+	printf("%0.2fkm/h Convertido em m/s e: %0.2f ",km, ms);
+	return 0;
+}
---
+++ Lista1ex36.c
@@ -0,0 +1,5 @@
+/*37. Ler uma velocidade em 𝑚/𝑠 (metros por segundo) e apresentá-la convertida em
+𝐾𝑚/ℎ (quilômetros por hora). A fórmula de conversão é: 𝐾 = 𝑀 ∗ 3,6 , sendo 𝐾 a velocidade
+em 𝑘𝑚/ℎ e 𝑀 em 𝑚/𝑠.
+*/
+
